94a2a0f03a8f6ce1e480297cc47a70c90105dd52
root <root@algorici.localdomain>
2016-10-15 01:59:33 +0000

    added database schema

---
+++ dbInit.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Users
+(
+id INTEGER,
+nickname TEXT,
+name TEXT,
+firstName TEXT,
+promo INTEGER,
+submitInQueue INTEGER,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE Problems
+(
+id INTEGER,
+userId INTEGER,
+maxScore REAL,
+FOREIGN KEY(userId) REFERENCES Users(id),
+PRIMARY KEY(id,userId)
+);
+
+CREATE TABLE Submissions
+(
+id INTEGER,
+userId INTEGER,
+problemId INTEGER,
+score REAL,
+PRIMARY KEY(id),
+FOREIGN KEY(userId) REFERENCES Users(id),
+FOREIGN KEY(problemId) REFERENCES Problems(id)
+);
